fc329896597f5044b3e66abe81ddb172743a9477
LiBai <shapemind@163.com>
2021-12-05 23:03:26 +0800

    20211205-MPC

---
+++ myclass04/mycode05_preinpostraversal.py
@@ -0,0 +1,12 @@
+class Node:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.value = data
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
