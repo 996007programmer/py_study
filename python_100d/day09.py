c5b05fc1a46f5569a5bb93d89b1014bd8659eeee
shapemind@aliyun.com <shapemind@163.com>
2021-12-20 18:50:49 +0800

    20211220-CPC-COMMIT

--- python_100d/day09.py
+++ python_100d/day09.py
@@ -1,5 +1,5 @@
-#author:shapemind
-#dt: 2021/12/14 14:06
+# author:shapemind
+# dt: 2021/12/14 14:06
 # class Person(object):
 #
 # 	def __init__(self, name, age):
@@ -26,6 +26,7 @@
 from abc import ABCMeta, abstractmethod
 from random import randint, randrange
 
+
 class Fighter(object, metaclass=ABCMeta):
 	"""战斗属性"""
 	__slots__ = ('_name', '_hp')
@@ -37,12 +38,15 @@ class Fighter(object, metaclass=ABCMeta):
 	@property
 	def name(self):
 		return self._name
+	
 	@property
 	def hp(self):
 		return self._hp
+	
 	@hp.setter
 	def hp(self, hp):
 		self._hp = hp if hp >= 0 else 0
+	
 	@property
 	def alive(self):
 		return self._hp > 0
@@ -51,6 +55,7 @@ class Fighter(object, metaclass=ABCMeta):
 	def attack(object, other):
 		"""攻击"""
 		pass
+
 
 class Ultraman(Fighter):
 	"""奥特曼"""
@@ -68,7 +73,7 @@ class Ultraman(Fighter):
 			self._mp -= 20
 			for temp in others:
 				if temp.alive:
-					temp.hp -= randint(10,15)
+					temp.hp -= randint(10, 15)
 			return True
 		else:
 			return False
@@ -80,8 +85,9 @@ class Ultraman(Fighter):
 	
 	def __str__(self):
 		return '~~~%s奥特曼~~~' % self._name + \
-			'生命值：%s' % self._hp + \
-			'魔法值：&s' % self._mp
+		       '生命值：%s' % self._hp + \
+		       '魔法值：&s' % self._mp
+
 
 class Monster(Fighter):
 	"""怪兽"""
@@ -89,23 +95,26 @@ class Monster(Fighter):
 	
 	def attack(object, other):
 		other.hp -= randint(10, 20)
-		
+	
 	def __str__(self):
 		return '~~~%s小怪兽~~~\n' % self._name + '生命值：%d\n' % self._hp
-	
+
+
 def is_any_alive(monsters):
 	for monster in monsters:
 		if monster.alive > 0:
 			return True
 	return False
 
+
 def select_alive_one(monsters):
 	monsters_len = len(monsters)
 	while True:
-		index = randrange(-1,monsters_len)
+		index = randrange(-1, monsters_len)
 		monster = monsters[index]
 		if monster.alive > 0:
 			return monster
+
 
 def display_info(ultramans, monsters):
 	"""显示奥特曼和小怪兽的信息"""
@@ -125,21 +134,22 @@ class PersonSlot(object):
 	@property
 	def name(self):
 		return self._name
-
+	
 	@property
 	def age(self):
 		return self._age
-
+	
 	@name.setter
 	def name(self, name):
 		self._name = name
-
+	
 	@age.setter
 	def age(self, age):
 		self._age = age
-		
+
+
 class Triangle(object):
-	def __init__(self,a, b, c):
+	def __init__(self, a, b, c):
 		self._a = a
 		self._b = b
 		self._c = c
@@ -148,9 +158,11 @@ class Triangle(object):
 	def is_valid(a, b, c):
 		return a + b > c and b + c > a and a + c > b
 
+
 """
 类方法
 """
+
 
 class Clock(object):
 	def __init__(self, hour=0, minute=0, second=0):
@@ -158,24 +170,25 @@ class Clock(object):
 		self._minute = minute
 		self._second = second
 	
-	
 	@classmethod
 	def now(cls):
 		from time import time, localtime, sleep
 		ctime = localtime(time())
 		return cls(ctime.tm_hour, ctime.tm_min, ctime.tm_sec)
-
+	
 	def show(self):
 		return '%02d:%02d:%02d' % (self._hour, self._minute, self._second)
+
 
 class Person(object):
 	def __init__(self, name, age):
 		self._name = name
 		self._age = age
-		
+	
 	@property
 	def name(self):
 		return self._name
+	
 	@property
 	def age(self):
 		return self._age
@@ -183,21 +196,26 @@ class Person(object):
 	@name.setter
 	def name(self, name):
 		self._name = name
+	
 	@age.setter
 	def age(self, age):
 		self._age = age
-	
+
+
 class Student(Person):
 	def __init__(self, name, age, grade):
 		super().__init__(name, age)
 		self._grade = grade
-		
+	
 	@property
 	def grade(self):
 		return self._grade
+	
 	@grade.setter
 	def grade(self, grade):
 		self._grade = grade
+
+
 class Teacher(Person):
 	def __init__(self, name, age, title):
 		super().__init__(name, age)
@@ -206,33 +224,41 @@ class Teacher(Person):
 	@property
 	def title(self):
 		return self._title
+	
 	@title.setter
 	def title(self, title):
 		self._title = title
-		
+
+
 from abc import ABCMeta, abstractmethod
+
+
 class Pet(object, metaclass=ABCMeta):
 	"""宠物"""
+	
 	def __init__(self, nickname):
 		self._nickname = nickname
-		
+	
 	@abstractmethod
 	def make_voice(self):
 		"""发出声音"""
 		pass
 
+
 class Dog(Pet):
 	"""狗"""
+	
 	def make_voice(self):
 		print('%s：汪汪汪' % self._nickname)
-	
+
+
 class Cat(Pet):
 	"""猫"""
+	
 	def make_voice(self):
 		print('%s:喵喵喵~~' % self._nickname)
 
 
-	
 def main():
 	u = Ultraman('骆昊', 1000, 120)
 	m1 = Monster('狄仁杰', 250)
@@ -270,8 +296,8 @@ def main():
 	else:
 		print('小怪兽胜利!')
 	
-	
+	print()
 
 
 if __name__ == '__main__':
-    main()
+	main()
